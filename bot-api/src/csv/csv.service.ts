1ec726026a10a9460a2c3ede9c8bff9d85457278
jaac25 <acevedoalexander52@gmail.com>
2026-06-25 01:03:53 -0500

    Fix bug csv

--- bot-api/src/csv/csv.service.ts
+++ bot-api/src/csv/csv.service.ts
@@ -13,16 +13,13 @@ export class CsvService {
   private readonly logger = new Logger(CsvService.name);
   async read<T = Record<string, string>>(fileName: string): Promise<T[]> {
     try {
-      const path = join(process.cwd(), 'data', fileName);
-
+      const path = join(__dirname, '..', 'data', fileName);
       if (!existsSync(path)) {
         throw new InternalServerErrorException(`CSV file not found: ${path}`);
       }
 
       // The csv file is read and provides the rows list
       return new Promise((resolve, reject) => {
-        const path = join(process.cwd(), 'data', fileName);
-
         const rows: T[] = [];
 
         createReadStream(path)
@@ -41,11 +38,6 @@ export class CsvService {
           );
       });
     } catch (error) {
-      const path = join(process.cwd(), 'data', fileName);
-      console.log('cwd:', process.cwd());
-      console.log('path:', path);
-      console.log('exists:', existsSync(path));
-
       const axiosError = error as AxiosError;
       this.logger.error(
         `Error reading CSV file: "${fileName}": ${axiosError.message}`,
